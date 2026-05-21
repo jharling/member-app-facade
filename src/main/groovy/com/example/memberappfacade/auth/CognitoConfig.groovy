5d3eff847df6a5e5cbb82db9100096eac4e6a98f
James Harling <jharling@harlingcs.com>
2026-05-21 15:29:11 -0500

    Configure Cognito client region

--- src/main/groovy/com/example/memberappfacade/auth/CognitoConfig.groovy
+++ src/main/groovy/com/example/memberappfacade/auth/CognitoConfig.groovy
@@ -1,15 +1,19 @@
 package com.example.memberappfacade.auth
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
 
 @Configuration
 class CognitoConfig {
 
 	@Bean
-	CognitoIdentityProviderClient cognitoIdentityProviderClient() {
-		CognitoIdentityProviderClient.create()
+	CognitoIdentityProviderClient cognitoIdentityProviderClient(@Value('${aws.region}') String awsRegion) {
+		CognitoIdentityProviderClient.builder()
+				.region(Region.of(awsRegion))
+				.build()
 	}
 
 }
