92ce5c4433a41476313c4bdf7f176d952c3a2788
James Harling <jamesharling@Jamess-Mac-Studio.local>
2026-05-21 10:06:46 -0500

    Ignore local lib directory

--- src/main/groovy/com/example/memberappfacade/MemberAppFacadeApplication.groovy
+++ src/main/groovy/com/example/memberappfacade/MemberAppFacadeApplication.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class MemberAppFacadeApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(MemberAppFacadeApplication, args)
-    }
+	static void main(String[] args) {
+		SpringApplication.run(MemberAppFacadeApplication, args)
+	}
 
 }
--- src/test/groovy/com/example/memberappfacade/MemberAppFacadeApplicationTests.groovy
+++ src/test/groovy/com/example/memberappfacade/MemberAppFacadeApplicationTests.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class MemberAppFacadeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
