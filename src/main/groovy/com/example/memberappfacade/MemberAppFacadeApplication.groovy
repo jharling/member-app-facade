447338aa29d022dc994d45d3c9b7ce429fa74044
James Harling <jamesharling@Jamess-Mac-Studio.local>
2026-05-21 09:55:40 -0500

    Initial project setup

---
+++ src/main/groovy/com/example/memberappfacade/MemberAppFacadeApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.memberappfacade
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MemberAppFacadeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MemberAppFacadeApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/memberappfacade/MemberAppFacadeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.memberappfacade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemberAppFacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
